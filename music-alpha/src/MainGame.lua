f57015fa08bc9e899a370cb9d061cecfd4c4d677
longk <longkas@gmail.com>
2025-10-21 12:57:54 +0800

    add SyncElementState between server and client

--- music-alpha/src/MainGame.lua
+++ music-alpha/src/MainGame.lua
@@ -9,13 +9,13 @@ function CallbackCharCreated(playerId)
 end
 
 function InitClient() 
-    print("BindNotify ")
+    print("BindNotify InitClient")
     System:BindNotify(msgIdBlockState, OnClientNotify)
 end
 
 function InitServer() 
     -- RegisterEventsServer()
-    TimerManager:AddTimer(UMath:GetRandomInt(1,10), PlaySfx, "levelcomplete")
+    -- TimerManager:AddTimer(UMath:GetRandomInt(1,10), PlaySfx, "levelcomplete")
     InitServerTimers()
 end
 
@@ -23,28 +23,84 @@ function InitServerTimers()
     TimerManager:AddLoopTimer(5, GenBlock)
 end
 
-function OnClientNotify(msgId, msg)
-    print("OnClientNotify ", elementId)
-    if msgId == msgIdBlockState then
-        SetBlockState(msg)
+function GetElementState(elementId)
+    return {eid=elementId}
+end
+
+function SetElementStateColor(state, idx, color)
+    if state.colors == nil then
+        state.colors = {}
+    end
+    table.insert(state.colors, {n=idx, c=color})
+end
+
+function SetElementStatePhy(state, phyAffectForce, phyCarrible, phyColliChar)
+    state.phys = {phyAffectForce, phyCarrible, phyColliChar}
+end
+
+function SetElementStateColli(state, enableColli)
+    state.colls = enableColli
+end
+
+function SetElementStateMass(state, massNum)
+    state.mass = massNum
+end
+
+
+function BuildBlockState(elementId, color1)
+    local state = GetElementState(elementId)
+    SetElementStateColor(state, 1, color1)
+    SetElementStatePhy(state, true, true, true)
+    SetElementStateColli(state, true)
+    SetElementStateMass(state, 20)
+    -- print("BuildBlockState ", MiscService:Table2JsonStr(state))
+    return state
+end
+
+--同步元件状态
+function SyncElementState(state)
+    -- print("SyncElementState ", MiscService:Table2JsonStr(state))
+    local elementId = state.eid
+
+    if state.colors ~= nil then
+        -- print("SyncElementState setcolor", MiscService:Table2JsonStr(state.colors))
+        for key, value in pairs(state.colors) do
+            Element:SetColor(elementId, value.n, value.c)
+        end
+    end
+
+    if state.phys ~= nil then
+        Element:SetPhysics(elementId, state.phys[1], state.phys[2], state.phys[3])
+    end
+
+    if state.colls ~= nil then
+        Element:SetEnableCollision(elementId, state.colls)
+    end
+
+    if state.mass ~= nil then
+        Element:SetMass(elementId, state.mass)
     end
 end
 
-function SetBlockState(elementId)
-    print("SetBlockState ", elementId)
-    local color = getRandomColorRGBA()
-    Element:SetColor(elementId, 1, color)
-    Element:SetPhysics(elementId, true, true, true)
-    Element:SetEnableCollision(elementId, true)
-    Element:SetMass(elementId, 20)
+function OnClientNotify(msgId, msg)
+    print("OnClientNotify ", MiscService:Table2JsonStr(msg))
+    if msgId == msgIdBlockState then
+        if not System:IsStandalone() then
+            SyncElementState(msg)
+        end
+    end
 end
+
+
 
 function GenBlock() 
     local callback = function(elementId)
         print("SpawnElement res ", elementId)
-        System:SendToAllClients(msgIdBlockState, elementId)
-        SetBlockState(elementId)
-        Element:DestroyByTime(elementId, 8)
+        local color = getRandomColorRGBA()
+        local state = BuildBlockState(elementId, color)
+        System:SendToAllClients(msgIdBlockState, state)
+        SyncElementState(state)
+        Element:DestroyByTime(elementId, 15)
 
         -- CustomProperty:SetCustomProperty(elementId, "musicVec", CustomProperty.PROPERTY_TYPE.Vector, Engine.Vector(0, trackIdx, noteIdx))
     end
